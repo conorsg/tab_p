4c18aeed43367d6f2dcdf5e4c330a0207a585a0f
Conor <conorsg@gmail.com>
2014-05-06 11:01:03 -0500

    merge from dev branch

--- app.js
+++ app.js
@@ -1,55 +1,36 @@
-// Search your open tabs
-// Made by Conor Gaffney, 2012
-
+// Refactored 2014
+// Dependencies: qsScore, jQuery
 (function ($, win, doc) {
+  var tabs;
 
-  var tabsData = [];
+  chrome.tabs.query(
+    { currentWindow: true},
+    function(e) { tabs = e }
+  );
 
-  //returns array of tabs objects
-  function getTabs() {
-    chrome.tabs.getAllInWindow(function(tabList) {
-        window.tabs = tabList;
-        tabList.score = '';
-        tabsData = tabList;
-    });
+  function query(input) {
+    tabs.forEach(function(t) {
+      t.titleScore = t.title.toLowerCase().score(input)
+      t.urlScore = t.url.score(input)
+      t.score = t.titleScore + (t.urlScore * 0.75) //url match not as important as title
+    })
+
+    tabs.sort(function(a,b) {
+      return b.score - a.score
+    })
   }
 
-  //runs search term against tab properties using qs_score.js (http://code.google.com/p/rails-oceania/source/browse/lachiecox/qs_score/trunk/qs_score.js)
-  function runQuery(input) {
-    
-    for (var i = 0; i < tabsData.length; i++) {
-      titleQuery = tabsData[i].title.toLowerCase().score(input);
-      urlQuery = tabsData[i].url.score(input);
-
-      //weight title match over url match and set each tab's score
-      tabsData[i].score = titleQuery; 
-    }
-
-    tabsData.sort(function(a,b){
-      return b.score - a.score;
-    });
+  function list(tabs) {
+    tabs.forEach(function(t) {
+      $('ul').append( '<li><img src="'+t.favIconUrl+'" /><a href="#">'+t.title+'</a></li>' )
+      $('a').eq(tabs.indexOf(t)).click(function(e) {
+        chrome.tabs.update(t.id, updateProperties = { active: true })
+      })
+    })
   }
 
-  //writes ordered list of tabs
-  function writeList(tabsData){
-    for (var i = 0; i < tabsData.length; i++) {
-      $('ul').append('<li><img src="'+tabsData[i].favIconUrl+'" /><a href="#">'+tabsData[i].title+'</a></li>');
-      ( function () {
-        var x = i;
-        $('a').eq(x).click(function(e) {
-          openTab(x);
-        });
-      }) ();    
-    }
-  }
-
-  function openTab(i) {
-    //opens [i] tab search result
-    chrome.tabs.update(tabsData[i].id, updateProperties = {active:true});
-  }
-
-  // navigate through tab list with arrow keys
-  $(doc).ready(function() {
+  function init() {
+    // navigate through tab list with arrow keys
     $(win).keydown(function(e){
       start = $('.selected');
       start.removeClass('selected');
@@ -72,28 +53,24 @@
         }
       }
     });
-  });    
 
-  //let's do this
-  getTabs();
-
-  $(doc).ready(function() {
     $('#target').submit(function(e) {
-     
       e.preventDefault();
+
       var input = $('input[type="text"]').val();
       $('ul').empty();
 
-      if (input === '') {
+      if (input == '') {
         $('ul').append('<li class ="empty">Please enter a search term</li>');
-      } 
+      }
       else {
-        runQuery(input);
-        writeList(tabsData);
+        query(input);
+        list(tabs);
         $('li:first-child').addClass('selected');
         $('.selected a').focus();
       }
     });
-  });
+  }
 
+  $(doc).ready(function() { init(); });
 })(jQuery, window, document);
